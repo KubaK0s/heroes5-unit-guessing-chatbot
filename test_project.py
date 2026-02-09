b3d012300cf9e95b11d4fa982da614d7eb69856e
KubaK0s <72342359+KubaK0s@users.noreply.github.com>
2026-02-09 22:29:01 +0100

    Update test_project.py

--- test_project.py
+++ test_project.py
@@ -7,14 +7,14 @@ from main import load_bestiary
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 def test_pdf_exists():
-    # Sprawdzamy ścieżkę wewnątrz folderu
     expected_path = os.path.join("Bestiariusz", "Heroes5_bestiary.pdf")
     assert os.path.exists(expected_path), f"Nie znaleziono pliku PDF w: {expected_path}"
 
 def test_load_bestiary_missing_file():
-    splitter = RecursiveCharacterTextSplitter(chunk_size=100)
+    splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=0)
+    
     try:
         load_bestiary("nieistniejacy_plik.pdf", splitter)
-        assert False  
+        assert False 
     except FileNotFoundError:
         assert True  
